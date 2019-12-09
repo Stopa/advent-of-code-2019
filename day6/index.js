6578fa393dd9faa1cff78bdd52c9f868db06c1d6
Stepan Bolotnikov <stepan.bolotnikov@proekspert.ee>
2019-12-09 23:06:54 +0200

    day6 part2

--- day6/index.js
+++ day6/index.js
@@ -25,4 +25,33 @@ fs.readFile('./input.txt', (e, data) => {
   }, 0);
 
   console.log(orbitsCount);
+
+  const walkTo = (obj, target = 'COM') => {
+    const result = [obj];
+    if (orbiters[obj] === target) {
+      return result;
+    }
+
+
+    return [
+      ...result,
+      ...walkTo(orbiters[obj], target),
+    ];
+  }
+
+  const pathFromYOU = walkTo(orbiters['YOU']).reverse();
+  const pathFromSAN = walkTo(orbiters['SAN']).reverse();
+  let lastCommon;
+
+  for (let i = 0; i < pathFromYOU.length; i++) {
+    if (pathFromYOU[i] !== pathFromSAN[i]) {
+      break;
+    }
+
+    lastCommon = pathFromYOU[i];
+  }
+  
+  const pathLengthFromYOU = walkTo(orbiters['YOU'], lastCommon).length;
+  const pathLengthFromSAN = walkTo(orbiters['SAN'], lastCommon).length;
+  console.log(pathLengthFromSAN + pathLengthFromYOU);
 });
